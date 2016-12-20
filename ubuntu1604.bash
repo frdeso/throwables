595fc5ddbc8fed4071eee62d2cac6397de2c24ae
Francis Deslauriers <francis.deslauriers@efficios.com>
2016-12-20 17:29:36 -0500

    Complete Ubuntu 16.04 build profile
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>

--- ubuntu1604.bash
+++ ubuntu1604.bash
@@ -8,7 +8,10 @@ lxc launch ubuntu:16.04 $CONTAINER_NAME
 
 PACKAGES="bc libssl-dev zsh vim git make htop tree \
 	  software-properties-common python-dev python-pip \
-	  python3-dev python3-pip"
+	  python3-dev python3-pip autoconf automake sed flex bison \
+	  libtool liburcu-dev liburcu4 libelf-dev gcc ccache libpopt-dev \
+	  uuid-dev pkg-config libxml2-dev libglib2.0-dev swig libdwarf-dev \
+	  libdw-dev"
 
 echo "#####################"
 echo "#   Provisionning   #"
@@ -17,6 +20,7 @@ sleep 5
 lxc exec $CONTAINER_NAME -- apt update
 lxc exec $CONTAINER_NAME -- apt upgrade -y
 lxc exec $CONTAINER_NAME -- apt install -y $PACKAGES
+lxc exec $CONTAINER_NAME -- pip3 install vlttng
 
 echo ""
 echo "lxc exec $CONTAINER_NAME -- zsh"
